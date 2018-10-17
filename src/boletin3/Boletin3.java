95cd1eae8d17e2009f2058b2568f00026faf5e18
mrebelladofernandez <mrebelladofernandez@nautilus18.danielcastelao.org>
2018-10-17 12:36:25 +0200

    first commit

---
+++ src/boletin3/Boletin3.java
@@ -0,0 +1,19 @@
+package boletin3;
+
+public class Boletin3 {
+
+    public static void main(String[] args) {
+        //apartado a
+        Consumo consume1 = new Consumo();
+        //apartado b
+        consume1.setLitros(50);
+        consume1.setPGas(1.57);
+        //apartado c
+        Consumo consume2 = new Consumo(820, 51, 100, 1.33);
+        //apartado d
+        System.out.println("O consumo medio é" +consumoMedio);
+        //apartado e
+        //apartado f
+    }
+
+}
